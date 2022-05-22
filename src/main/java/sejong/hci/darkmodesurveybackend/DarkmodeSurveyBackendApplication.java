b074a467ed6ed4e7d13346750d297fbb126be182
Cho-D-YoungRae <yrc9229@gmail.com>
2022-05-22 20:57:42 +0900

    FEAT: BaseTimeEntity 설정

--- src/main/java/sejong/hci/darkmodesurveybackend/DarkmodeSurveyBackendApplication.java
+++ src/main/java/sejong/hci/darkmodesurveybackend/DarkmodeSurveyBackendApplication.java
@@ -2,7 +2,9 @@ package sejong.hci.darkmodesurveybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DarkmodeSurveyBackendApplication {
 
