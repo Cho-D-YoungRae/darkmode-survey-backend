32a49c64dca20ff5d920094d1c54bcf407177a2a
Cho-D-YoungRae <yrc9229@gmail.com>
2022-06-02 23:31:12 +0900

    FEAT: FindingWordAnswersDto answers NotNull validation 추가

--- src/main/java/sejong/hci/darkmodesurveybackend/dto/findingword/FindingWordAnswersDto.java
+++ src/main/java/sejong/hci/darkmodesurveybackend/dto/findingword/FindingWordAnswersDto.java
@@ -2,10 +2,12 @@ package sejong.hci.darkmodesurveybackend.dto.findingword;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class FindingWordAnswersDto {
 
+    @NotNull
     private List<FindingWordAnswerDto> answers;
 }
